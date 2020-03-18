39a5a2ad567dd98a56af7e2b1f6378ca405a4b76
33daysK <enjingorira@gmail.com>
2020-03-18 17:23:16 +0900

    refactoring : add comment

--- Option.kt
+++ Option.kt
@@ -1,10 +1,22 @@
 package jsv
 
 import kotlin.system.exitProcess
-
+/**  
+ *  Optionクラスです。
+ *  
+ *  @auther:33daysK
+ *  @version バージョン1.0 2020/03/18
+ *　 when文でOptionの種類を判断しています。
+ *  追加するときはwhen節の中に書き込んでください。
+ */
 fun option(option_item : String){
 	when(option_item){
-		"-help" -> println("アプリケーション「XXXX」は、JSON形式のファイル内の情報を入力キーを元に並べ替えて結果を表示します。")
-		else -> exitProcess(0)
+		"-help" -> {
+			println("アプリケーション「XXXX」は、CSV形式のファイル内の情報を入力キーを元に並べ替えて結果を表示します。")
+		}
+		else -> {
+			println("存在しないオプションです")
+			exitProcess(0)
+		}
 	}
 }
